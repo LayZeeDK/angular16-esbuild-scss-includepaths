59613a329651f32eaf3216467a0c93d73963c0b0
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2023-05-08 13:46:25 +0200

    feat: add Sass color variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular16-esbuild-scss-includepaths';
-}
+export class AppComponent {}
